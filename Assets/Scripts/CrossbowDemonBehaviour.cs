89542f3b390d055dd578e80d674800b4cce4dd64
Dominic Pham <dom17pham@gmail.com>
2022-08-10 17:52:36 -0400

    Modified Crossbow Demon Behavior
    Added a cooldown timer to control the projectile fire. Added a transformation of the arrow child object to move to the parent position.
    Made comments for clarity.

--- Assets/Scripts/CrossbowDemonBehaviour.cs
+++ Assets/Scripts/CrossbowDemonBehaviour.cs
@@ -1,24 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class CrossbowDemonBehaviour : MonoBehaviour
 {
 
     public ArrowProjectile ProjectilePrefab;
     public Transform LaunchOffset;
-
-    public GameObject arrow;
+    public GameObject Arrow;
     private SpriteRenderer sprite;
+    private float CooldownTimer;
+    private float Cooldown = 3;
     void Update()
     {
-        if(GameObject.Find("Arrow Projectile(Clone)") == null){
-        Instantiate(ProjectilePrefab, LaunchOffset.position, transform.rotation);
+        // Cooldown timer to set a time interval between each projectile
+        // Prevents constant projectile generation ie infinite projectile firing.
+        CooldownTimer -= Time.deltaTime;
 
-        arrow = GameObject.Find("Arrow_0");
-        arrow.transform.position = new Vector3(transform.position.x, transform.position.y, 0f);
-        sprite = arrow.GetComponent<SpriteRenderer>();
-        sprite.sortingLayerID = SortingLayer.NameToID("Projectile");
-        }
+        // Checks if the Cooldown time has been reached before resetting it
+        if (CooldownTimer > 0) return;
+        CooldownTimer = Cooldown;
+
+        // Create a new projectile game object 
+        Instantiate(ProjectilePrefab, LaunchOffset.position, transform.rotation);
+        
+        // Arrow is set to the child game object of the arrow projectile created above
+        Arrow = GameObject.Find("Arrow_0");
+        // Arrow child game object is moved to the same position as parent
+        Arrow.transform.localPosition = new Vector3(0, 0, 0);
     }
 }
