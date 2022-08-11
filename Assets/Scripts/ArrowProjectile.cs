fdf816d5d2ea808bac5e01a16c1bfcf1948ddab4
Dominic Pham <dom17pham@gmail.com>
2022-08-11 14:54:44 -0400

    Update ArrowProjectile.cs
    Added code that creates an array of all arrow projectiles present in the game scene and applies the same tag and sets z position of all projectiles to 0 so that it is visible in the game scene.
    
    Inline comments were also added to improve code clarity.

--- Assets/Scripts/ArrowProjectile.cs
+++ Assets/Scripts/ArrowProjectile.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 public class ArrowProjectile : MonoBehaviour
 {
     public float speed = 2.0f;
+    private GameObject Parent;
+    private GameObject Child;
+    private GameObject[] Parents;
+
+    void Start()
+    {
+        // Creates a list of all arrow projectile in game scene
+        if (Parents == null){
+            Parents = GameObject.FindGameObjectsWithTag("ParentProjectile");
+        }
+
+        // For each child of each arrow projectile, change tag to projectile 
+        // to differentiate it from its parent object and set child game object
+        // to the same position as the parent.
+        foreach(GameObject Parent in Parents){
+            Child = Parent.transform.GetChild(0).gameObject;
+            Child.gameObject.tag = "Projectile";
+            Child.transform.localPosition = new Vector3(0f, 0f, 0f);
+        }
+    }
     void Update()
     {
+        // Moves the arrow projectile to the right 
         transform.position += -transform.right * Time.deltaTime * speed;
     }
 
+    // Method below is used to destroy the projectile when it collides 
+    // with another 2d object or with the terrain
     private void OnCollisionEnter2D(Collision2D collision){
         Destroy(gameObject);
     }
